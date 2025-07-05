394c730181a1bd8001b81a9479944d6c17f9ba0a
twinlaboratories <jkibusiness@outlook.com>
2025-07-06 00:44:38 +0300

    Update next.config.mjs

--- creative-writer-portfolio/next.config.mjs
+++ creative-writer-portfolio/next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
